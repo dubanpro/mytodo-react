aeebc4da609c24aa5b55eb3c71cbd40020eca556
PROGRAMADOR IECOMM SAS <programador.iecomm@gmail.com>
2025-05-07 20:17:11 -0500

    Ajuste de finales de línea

---
+++ src/components/Title/Title.jsx
@@ -0,0 +1,10 @@
+//* Esta es la creacion de mi titulo Todo App importado en index.js de mi folder title */}
+export const Title = () => {
+    return (
+        <h1 className="text-5xl font-Briem Hand font-bold tracking-widest">
+        Todo App
+        </h1>
+    )
+
+
+}
